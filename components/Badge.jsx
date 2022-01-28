3e702f3c410a0624dc46bb61f6a0bfa9b6a50328
mooenz <joss92821@hotmail.com>
2022-01-27 22:34:41 -0500

    ADD color de fuente predeterminada

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
-
 const Badge = ({ name, logo, color, fontColor, alt, width, height }) => {
   return (
     <>
       <img
-        src={`https://img.shields.io/badge/${name}-${color}?style=flat&logo=${logo}&logoColor=${fontColor}`}
+        src={`https://img.shields.io/badge/${name}-${color}?style=flat&logo=${logo}&logoColor=${fontColor || 'white'}`}
         alt={alt}
         width={width}
         height={height}
