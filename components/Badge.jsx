8066bba829566d24fc03788c728d4a31d6e83368
mooenz <joss92821@hotmail.com>
2022-02-02 18:09:36 -0500

    Doy formato al archivo

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -2,7 +2,9 @@ const Badge = ({ name, logo, color, fontColor, alt, width, height }) => {
   return (
     <>
       <img
-        src={`https://img.shields.io/badge/${name}-${color}?style=flat&logo=${logo}&logoColor=${fontColor || 'white'}`}
+        src={`https://img.shields.io/badge/${name}-${color}?style=flat&logo=${logo}&logoColor=${
+          fontColor || 'white'
+        }`}
         alt={alt}
         width={width}
         height={height}
