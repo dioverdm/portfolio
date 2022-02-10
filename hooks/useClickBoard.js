b5e1a99cebbf94133b5158b88a3d15b16868f7fd
mooenz <joss92821@hotmail.com>
2022-02-10 11:22:15 -0500

    agrego nuevo hook

--- hooks/useClickBoard.js
+++ hooks/useClickBoard.js
@@ -3,7 +3,7 @@ import toast from 'react-hot-toast';
 export const useClickBoard = () => {
   navigator.clipboard.writeText('joss92821@hotmail.com');
 
-  return toast.success('Copiado!', {
+  return toast.success('Email Copiado!', {
     position: 'bottom-center',
     duration: 7000,
     style: {
