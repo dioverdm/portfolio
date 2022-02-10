4b0f8f28d74a01ffa80361ffe8cdcbc9e060bfd8
mooenz <joss92821@hotmail.com>
2022-02-10 13:56:15 -0500

    agrego nuevo componente

---
+++ components/Skills.jsx
@@ -0,0 +1,19 @@
+import data from '../constant/skills.json';
+import styles from '../styles/Skills.module.scss';
+
+const Skills = () => {
+  const { skills, skills__body, skills__name, skills__img } = styles;
+
+  return (
+    <ol className={skills}>
+      {data.map(({ name, icon, id }) => (
+        <li className={skills__body} key={id}>          
+          <img className={skills__img} src={icon} />
+          <p className={skills__name}>{name}</p>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default Skills;
