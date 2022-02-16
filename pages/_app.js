5c1e7d3bdf65d1c557f2d2dfd519b62ab0201cde
mooenz <joss92821@hotmail.com>
2022-02-15 21:49:45 -0500

    agrego estilos globales

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import { createContext } from 'react';
+
+//Styles
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
