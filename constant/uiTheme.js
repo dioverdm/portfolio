9ca2c1fdbdbebfb15d6094230c62d8b6bfabb4a4
mooenz <joss92821@hotmail.com>
2022-02-16 21:10:37 -0500

    agrego colores globales

---
+++ constant/uiTheme.js
@@ -0,0 +1,10 @@
+const Theme = {
+  'black-intense': '#111111',
+  black: '#222222',
+  'black-light': '#333333',
+  gray: '#bbb',
+  withe: '#eee',
+  'red-cherry': '#f53b57',
+};
+
+export default Theme;
