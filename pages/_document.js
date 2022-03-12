6cfb589a36df8caf1575fb281cfb8acb87f8d923
mooenz <joss92821@hotmail.com>
2022-03-11 21:19:58 -0500

    agrego imagen frontend mentor

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,21 @@ class MyDocument extends Document {
       <Html lang="es">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,700&display=swap" rel="stylesheet" />
-          <script
-            async
-            src={ `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}` }
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,700&display=swap"
+            rel="stylesheet"
           />
           <script
-            dangerouslySetInnerHTML={ {
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -23,7 +30,7 @@ class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
-            } }
+            }}
           />
         </Head>
         <body>
@@ -42,7 +49,7 @@ MyDocument.getInitialProps = async (ctx) => {
   try {
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App { ...props } />),
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
       });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -50,8 +57,8 @@ MyDocument.getInitialProps = async (ctx) => {
       ...initialProps,
       styles: (
         <>
-          { initialProps.styles }
-          { sheet.getStyleElement() }
+          {initialProps.styles}
+          {sheet.getStyleElement()}
         </>
       ),
     };
